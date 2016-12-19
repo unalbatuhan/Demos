05238f812994e3f8453a3a752f9ff496ff3b7b9e
Batuhan UNAL <unal.batuhan@outlook.com>
2016-12-19 02:51:02 +0200

    Commet
    Constructor and Encapsulation example

---
+++ ConstEncap/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstEncap
+{
+    class Program
+    {
+        class insan
+        {
+            //public string isim;
+            //public int kilo;
+            public string isim;
+            private int kilo;
+            public void setKilo(int kilo)
+            {
+                if(kilo<0)
+                {
+                    this.kilo = 0;
+                }
+                else
+                {
+                    this.kilo = kilo;
+                }
+                //this.kilo = kilo;
+            }
+            public int getKilo()
+            {
+                return kilo;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            insan ali = new insan();
+            ali.isim = "ali demir";
+           ali.setKilo (80);
+            insan veli = new insan();
+            //veli.kilo = 47;
+            Console.WriteLine("Batuhan UNAL");
+            //Console.WriteLine("ali'nin kilosu" + ali.kilo);
+            Console.WriteLine("alinin kilosu" + ali.getKilo());
+            ali.setKilo(-10);
+            Console.WriteLine("alinin kilosu" + ali.getKilo());
+            Console.ReadLine();
+        }
+    }
+}
