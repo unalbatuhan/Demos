f3e4efc4c6bfd44901536912af7ebbf0650eb3b6
Batuhan UNAL <unal.batuhan@outlook.com>
2017-01-03 13:30:14 +0000

    Commit

---
+++ MathCircleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MathCircleApp
+
+    //A program that finds the environment and area of the circle.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.142857143;
+            double space, environment, radius;
+            Console.Write("Enter the radius = ");
+            radius = Convert.ToInt32(Console.ReadLine());
+            environment = 2 * PI * radius;
+            space = PI * (Math.Pow(radius, 2));
+            Console.WriteLine("Environent area cm = " + environment);
+            Console.WriteLine("Circus Space Area cm = " + space);
+            Console.ReadLine();
+        }
+    }
+}
