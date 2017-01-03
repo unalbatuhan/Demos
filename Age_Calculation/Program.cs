93c655a476818451cae90b2b512cb0ef9a55dfc1
Batuhan UNAL <unal.batuhan@outlook.com>
2017-01-03 13:51:53 +0000

    Add files via upload

---
+++ Age_Calculation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Calculation
+{
+    
+  public  class Program
+    {
+        readonly int year = 2014;
+        public static  void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter Your Birthday :");
+            int birtday = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Age =" + (year-birtday));
+            Console.ReadKey();
+        }
+    }
+}
